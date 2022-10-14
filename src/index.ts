8e30ae3b03e1e66b8fdba78dd4c9543304d28c00
Jonatan Duttweiler <jonatanduttweiler@gmail.com>
2022-10-14 09:33:55 -0300

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,12 @@ import express from "express";
 import traceRouter from "./routes/api/v1/trace";
 import { Request, Response, NextFunction, Router } from "express";
 import Boom from '@hapi/boom';
+import cors from "cors";
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
